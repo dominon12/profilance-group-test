1b43d36e33beb6da282382edb688a31ab3d867d4
Max <youneedmax@gmail.com>
2021-12-08 12:17:06 +0100

    show greeting with user's login if user is authenticated and 'Гость' if not

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./Home.scss";
+import Title from "../Atoms/Title";
 
 /**
  * Home page
@@ -8,7 +10,14 @@ import "./Home.scss";
  * @return {*} {JSX.Element}
  */
 const Home = () => {
-  return <div>Home</div>;
+  const user = useSelector((state) => state.user);
+  const userName = user ? user.email : "Гость";
+
+  return (
+    <section className="home">
+      <Title>Привет, {userName}</Title>
+    </section>
+  );
 };
 
 export default Home;
